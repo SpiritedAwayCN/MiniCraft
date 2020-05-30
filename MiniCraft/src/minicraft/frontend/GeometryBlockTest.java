de9614f65d752dd3bce1506db2690af34694353f
SpiritedAwayCN <541845219@qq.com>
2020-05-30 10:30:52 +0800

    Add Textures

--- MiniCraft/src/minicraft/frontend/GeometryBlockTest.java
+++ MiniCraft/src/minicraft/frontend/GeometryBlockTest.java
@@ -8,23 +8,23 @@ import com.jme3.system.AppSettings;
 
 public class GeometryBlockTest {
 
-	@Test
-	public void testHash() {
-		// 配置参数
-		AppSettings settings = new AppSettings(true);
-		settings.setTitle("MiniCraft");
-		settings.setResolution(1024, 768);
-		settings.setResizable(true);
+	// @Test
+	// public void testHash() {
+	// 	// 配置参数
+	// 	AppSettings settings = new AppSettings(true);
+	// 	settings.setTitle("MiniCraft");
+	// 	settings.setResolution(1024, 768);
+	// 	settings.setResizable(true);
 				
-		// 启动jME3程序
-		TestApplication app = new TestApplication();
-		app.setSettings(settings);// 应用参数
-		app.setShowSettings(false);
-		app.start();
+	// 	// 启动jME3程序
+	// 	TestApplication app = new TestApplication();
+	// 	app.setSettings(settings);// 应用参数
+	// 	app.setShowSettings(false);
+	// 	app.start();
 		
-		//出于一些非常坑爹的原因，GeometryBlock根本没办法用JUnit调试，气skr人
-		assertEquals(null,app.getAssetManager());
-		//GeometryBlock.initialize(app.getAssetManager());
-	}
+	// 	//出于一些非常坑爹的原因，GeometryBlock根本没办法用JUnit调试，气skr人
+	// 	assertEquals(null,app.getAssetManager());
+	// 	//GeometryBlock.initialize(app.getAssetManager());
+	// }
 
 }
